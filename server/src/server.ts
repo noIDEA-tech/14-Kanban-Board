0c6fc52a739b11d7ab5272728848b8be73d98694
Nancy Watreas <nwatreas2023@gmail.com>
2025-01-22 11:24:05 -0600

    Add environment configuration files and update server setup with CORS support

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,26 @@
 const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
+app.use(express.json());
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
-
-app.use(express.json());
 app.use(routes);
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
