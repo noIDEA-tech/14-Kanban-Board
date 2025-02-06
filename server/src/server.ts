a9290e383f73aed9ba62840f03ac9155d1fe50d8
Nancy Watreas <nwatreas2023@gmail.com>
2025-02-06 16:49:27 -0600

    Fix path for serving static files and clean up commented code in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
 const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import routes from './routes/index.js';
+import path from 'path';
 import { sequelize } from './models/index.js';
 
 dotenv.config();
@@ -19,8 +24,12 @@ app.use(express.static('../client/dist'));
 app.use(routes);
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
+
+// app.get('*', (_req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// });
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
